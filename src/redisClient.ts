36d491e6ac595f73efd8be53bcf81c6460c79e79
Alexander Sung <alexsung98@gmail.com>
2025-03-05 19:36:09 -0800

    redis connection debugging

--- src/redisClient.ts
+++ src/redisClient.ts
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+console.log("Starting Redis client...");
 console.log("Connecting to Redis at", process.env.REDIS_URL);
 
 const redis = new Redis(process.env.REDIS_URL!);
