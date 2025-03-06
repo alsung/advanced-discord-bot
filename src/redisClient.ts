728622c059707ed965a6cbe0702a26f1d1d54632
Alexander Sung <alexsung98@gmail.com>
2025-03-05 20:33:25 -0800

    remove unnecessary print statements

--- src/redisClient.ts
+++ src/redisClient.ts
@@ -5,7 +5,6 @@ import dotenv from "dotenv";
 dotenv.config();
 
 console.log("Starting Redis client...");
-console.log("Connecting to Redis at", process.env.REDIS_URL);
 
 const redis = new Redis(process.env.REDIS_URL!);
 
